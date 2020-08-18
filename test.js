aa95c4488693093344729062b0f919a9ae5bf86a
scottcm73 <scottcmcmahan@gmail.com>
2020-08-17 21:57:50 -0500

    Added variables to map

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 // The svg
+
 var svg = d3.select("svg"),
   width = +svg.attr("width"),
   height = +svg.attr("height");
@@ -7,19 +8,24 @@ var svg = d3.select("svg"),
 var path = d3.geoPath();
 var projection = d3.geoMercator()
   .scale(200)
-
   .translate([width / 2, height / 2]);
 
+
+var maincsv="resources/2015_with_codes.csv"
+var keyval="happiness_score";
 // Data and color scale
 var data = d3.map();
 var colorScale = d3.scaleSequential()
   .domain([0,10])
   .interpolator(d3.interpolateRainbow);
-
+console.log(keyval)
 // Load external data and boot
 d3.queue()
+//  .defer(d3.json, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson")
+//  .defer(d3.csv, "maincsv", function(d){data.set(d.code, +d[keyval]); })
   .defer(d3.json, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson")
-  .defer(d3.csv, "resources/2015_with_codes.csv", function(d) { data.set(d.code, +d.happiness_score); })
+  .defer(d3.csv, maincsv, function(d) { data.set(d.code, +d[keyval]); })
+  
   .await(ready);
 
 console.log(data)
